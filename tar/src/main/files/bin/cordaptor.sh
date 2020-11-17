ce6d867b1e4976ce7e54b8c47567df2b0678c7bc
Igor Lobanov <igor@bond180.com>
2020-11-17 14:31:58 +0000

    default log4j config file is part of the standalone bundle

--- tar/src/main/files/bin/cordaptor.sh
+++ tar/src/main/files/bin/cordaptor.sh
@@ -10,6 +10,6 @@ export JAVA_APP_DIR="$(home_dir)"
 export JAVA_CLASSPATH="${JAVA_APP_DIR}/lib/*:${JAVA_APP_DIR}/ext/*:${JAVA_APP_DIR}/cordapps/*"
 export JAVA_MAJOR_VERSION=8
 export JAVA_MAIN_CLASS=tech.b180.cordaptor.kernel.ContainerKt
-export JAVA_OPTIONS="-Dconfig.file=${JAVA_APP_DIR}/conf/cordaptor.conf"
+export JAVA_OPTIONS="-Dconfig.file=${JAVA_APP_DIR}/conf/cordaptor.conf -Dlog4j.configurationFile=${JAVA_APP_DIR}/conf/log4j2.properties"
 
 $JAVA_APP_DIR/bin/run-java.sh
