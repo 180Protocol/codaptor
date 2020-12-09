281a407d8eaeb667e4404ed8757988fcece2ef13
Igor Lobanov <igor@bond180.com>
2020-12-09 19:05:36 +0000

    #7: added a queryable state to the test suite

--- reference-cordapp/src/main/kotlin/tech/b180/ref_cordapp/ReferenceCordapp.kt
+++ reference-cordapp/src/main/kotlin/tech/b180/ref_cordapp/ReferenceCordapp.kt
@@ -5,6 +5,9 @@ import net.corda.core.contracts.*
 import net.corda.core.flows.*
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import net.corda.core.schemas.QueryableState
 import net.corda.core.serialization.CordaSerializable
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.core.transactions.TransactionBuilder
@@ -14,6 +17,7 @@ import tech.b180.ref_cordapp.DelayedProgressFlow.Companion.SLEEPING
 import tech.b180.ref_cordapp.DelayedProgressFlow.Companion.WORKING
 import java.time.Duration
 import java.util.*
+import javax.persistence.*
 
 @BelongsToContract(TrivialContract::class)
 data class SimpleLinearState(
@@ -24,6 +28,89 @@ data class SimpleLinearState(
     get() = listOf(participant)
 }
 
+object ComplexStateSchema
+
+@Suppress("unused")
+object ComplexStateSchemaV1 : MappedSchema(
+    schemaFamily = ComplexStateSchema.javaClass,
+    version = 1,
+    mappedTypes = listOf(
+        PersistentComplexState::class.java,
+        PersistentComplexStateEntry::class.java
+    )
+) {
+
+  @Entity
+  @Table(name = "complex_states")
+  class PersistentComplexState(
+      @Column(name = "participant_name", nullable = false)
+      val participant: Party,
+
+      @Column(name = "string_value", nullable = false, length = 200)
+      val string: String,
+
+      @Column(name = "integer_value", nullable = false)
+      val integer: Int,
+
+      @Column(name = "amount_quantity", nullable = false)
+      val quantity: Long,
+
+      @OneToMany(targetEntity = PersistentComplexStateEntry::class, mappedBy = "parent", cascade = [CascadeType.REMOVE])
+      val nestedEntries: MutableList<PersistentComplexStateEntry>
+  ) : PersistentState() {
+    constructor(state: ComplexState) : this(participant = state.participant, string = state.string,
+        integer = state.integer, quantity = state.amount.quantity,
+        nestedEntries = mutableListOf()) {
+
+      state.nestedEntries.mapTo(nestedEntries) { PersistentComplexStateEntry(this, it) }
+    }
+  }
+
+  @Entity
+  @Table(name = "complex_state_entries")
+  class PersistentComplexStateEntry(
+      @ManyToOne(optional = false)
+      val parent: PersistentComplexState,
+
+      @Column(name = "string_value", nullable = false, length = 200)
+      val string: String,
+
+      @Column(name = "integer_value", nullable = false)
+      val integer: Int,
+
+      @Column(name = "amount_quantity", nullable = false)
+      val quantity: Long
+
+  ) : PersistentState() {
+    constructor(parent: PersistentComplexState, entry: ComplexState.Entry) : this(
+        parent = parent, string = entry.string, integer = entry.integer,
+        quantity = entry.amount.quantity)
+  }
+}
+
+@BelongsToContract(TrivialContract::class)
+data class ComplexState(
+    val participant: Party,
+    val string: String,
+    val integer: Int,
+    val amount: Amount<Currency>,
+    val nestedEntries: List<Entry> = emptyList()
+) : QueryableState {
+
+  override val participants: List<AbstractParty> get() = listOf(participant)
+
+  override fun supportedSchemas(): Iterable<MappedSchema> = listOf(ComplexStateSchemaV1)
+
+  override fun generateMappedObject(schema: MappedSchema) = ComplexStateSchemaV1.PersistentComplexState(this)
+
+  data class Entry(
+      val string: String,
+      val integer: Int,
+      val amount: Amount<Currency>
+  )
+}
+
+/** Contract that accepts any transaction */
 class TrivialContract : Contract {
   override fun verify(tx: LedgerTransaction) {
   }
@@ -117,4 +204,30 @@ open class DelayedProgressFlow(
 data class SimpleFlowResult(
     val output: StateAndRef<SimpleLinearState>,
     val feePaid: Amount<Currency>
+)
+
+@InitiatingFlow
+@StartableByRPC
+@StartableByService
+@Suppress("UNUSED")
+class IssueComplexStateFlow(
+    private val state: ComplexState
+) : FlowLogic<ComplexFlowResult>() {
+
+  override fun call(): ComplexFlowResult {
+    val builder = TransactionBuilder(notary = serviceHub.networkMapCache.notaryIdentities.first())
+    builder.addOutputState(state)
+    builder.addCommand(TrivialContract.Commands.RecordState(), ourIdentity.owningKey)
+
+    val pstx = serviceHub.signInitialTransaction(builder, ourIdentity.owningKey)
+    val fstx = subFlow(FinalityFlow(pstx, emptySet<FlowSession>()))
+
+    return ComplexFlowResult(fstx.tx.outRef(0))
+  }
+}
+
+// FIXME serializer cannot correctly generate flow result type schema for FlowLogic<StateAndRef<ComplexState>>
+@CordaSerializable
+data class ComplexFlowResult(
+    val output: StateAndRef<SimpleLinearState>
 )
