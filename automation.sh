1f1a300947edde838f313b83f366f528d942ec97
Patrick <patricklouie.pl@gmail.com>
2021-04-23 17:37:14 +0800

    resolves #1 added support for different version of Corda 4. added multi-build process automation.

--- automation.sh
+++ automation.sh
@@ -1,16 +1,25 @@
-#Get list of configurations in Configurations file
-configurations=("4.5" "4.6" "4.7")
+#Variable that houses the list of supported Corda Version(left) alongside its Corda platform version(right) counterpart
+declare -A supportedCordaVersions=(
+  [4.4]=6
+  [4.5]=7
+  [4.6]=8
+  [4.7]=9
+)
+
+#Identifies the version/s of corda to be built and tested
+if [ "$1" == "" ]; then
+configurations=("${!supportedCordaVersions[@]}")
+elif [[ "${!supportedCordaVersions[*]}" =~ $1 ]]; then
+configurations=$1
+else
+echo "Version entered not supported"
+read junk
+  exit 1
+fi
 
 for configuration in "${configurations[@]}"; do
 
-  #If-Else statement to check tag the platform version based on corda version
-  if [ "$configuration" == "4.5" ]; then
-      platform_version=7;
-  elif [ "$configuration" == "4.6" ]; then
-      platform_version=8;
-  elif [ "$configuration" == "4.7" ]; then
-      platform_version=9;
-  fi
+  echo -e "\033[0;31mBuilding Cordaptor Version $configuration"
 
   #Replaces version of Corda in gradle.properties file
   sed -i "/corda_core_release_version/c\corda_core_release_version=$configuration" ./gradle.properties
@@ -18,9 +27,8 @@ for configuration in "${configurations[@]}"; do
   sed -i "/cordaptor_version/c\cordaptor_version=0.2.0-corda$configuration-SNAPSHOT" ./gradle.properties
 
   #Replaces target and minimum platform version in build.gradle under reference-cordapp project
-  sed -i "/targetPlatformVersion/c\ \ttargetPlatformVersion $platform_version" ./reference-cordapp/build.gradle
-  sed -i "/minimumPlatformVersion/c\ \tminimumPlatformVersion $platform_version" ./reference-cordapp/build.gradle
-
+  sed -i "/targetPlatformVersion/c\ \ttargetPlatformVersion ${supportedCordaVersions[$configuration]}" ./reference-cordapp/build.gradle
+  sed -i "/minimumPlatformVersion/c\ \tminimumPlatformVersion ${supportedCordaVersions[$configuration]}" ./reference-cordapp/build.gradle
 
   #Build with imported configurations
   ./gradlew build
@@ -29,7 +37,7 @@ for configuration in "${configurations[@]}"; do
   ./gradlew --stop
 
   #Gradle task to create and push docker image of current Corda Version
-  if [ "$1" == "dockerPush" ]; then
+  if [ "$2" == "dockerPush" ]; then
     ./gradlew :tar:dockerPush
   else
     ./gradlew :tar:docker
@@ -40,4 +48,5 @@ for configuration in "${configurations[@]}"; do
 done
 
 #Used to prompt the user to press "Enter" before exiting the terminal
+echo -e "\033[0;31mAutomated Building Done!"
 read junk
--- docs/developer-guide.md
+++ docs/developer-guide.md
@@ -1,11 +1,22 @@
 # Developer Guide
 
-## Automated Building (Corda version 4.5-4.7)
+## Automated Building (Corda version 4.4-4.7)
 
-By using the included `automation.sh` file in the root directory you could automatically build and test the Cordaptor through Corda versions 4.5 to 4.7
+By using the included `automation.sh` file in the root directory you could automatically build and test the Cordaptor through Corda versions 4.4 to 4.7
 (This could be changed by changing the `configurations` variable found inside the `automation.sh` file).
-The `dockerPush` argument could be added which enables the shell scripts ability to push the docker build directly to the remote repository.
-### Example
+
+Automation.sh could accept 2 arguments:
+###Argument #1
+A first argument could be passed to build a specific version of the cordaptor. The example below will automatically build the cordaptor on Corda version 4.5.
+#### Example
 ```
-./automation.sh dockerPush
+./automation.sh 4.5
+```
+###Argument #2
+The `dockerPush` argument could be added which enables the shell script's ability to push the docker build directly to the remote repository. 
+
+Note: You have to pass `""` on the first argument if you want the default to build all Corda Versions supported.
+#### Example
+```
+./automation.sh "" dockerPush
 ```
